161b6d4ad3b636cfb32e68fc353695f858151081
User <mehrotraatharv15@gmail.com>
2026-01-11 02:57:04 +0530

    polish  commit

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Cyber Portfolio API is running');
+  res.send('Cyber Portfolio API is running successfully.');
 });
 
 app.post('/api/contact', async (req, res) => {
